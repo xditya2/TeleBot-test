8868550f843858ba6209b521949957c9b7ce8fd7
xditya <58950863+xditya@users.noreply.github.com>
2020-02-22 19:44:25 +0530

    Initial commit

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,17 @@
+""".admin Plugin for @XtraTgBot"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("**`Hey! I'm alive. All systems online and functioning normally... ψ(｀∇´)ψ`**\n\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @xditya\n`Database Status: Databases functioning normally!\n\nI will be always with you, my master!\n`"
+                     f"`My owner`: {DEFAULTUSER}\n"
+                     "[Deploy this userbot Now](https://github.com/xditya/X-tra-Telegram)")
